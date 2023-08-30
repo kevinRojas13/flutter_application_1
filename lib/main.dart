6dbc472f2e47720586d3748c01b1144a4a0a9696
kevinRojas13 <202000700b@utea.edu.pe>
2023-08-29 19:40:12 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xffFBF0B2),
+        body: SafeArea(
+          child: Text(
+            'Hola',
+            style: TextStyle(
+              color: Colors.black, // Cambiado a un color válido, por ejemplo, negro.
+              fontSize: 30.0,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
