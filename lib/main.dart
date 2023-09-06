2f804c4fe4161b505187e29f48e018be908b34f4
kevinRojas13 <202000700b@utea.edu.pe>
2023-09-06 01:14:12 -0500

    agregué iamgen centrada, y texto debajo

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Obtener la fecha actual
+    DateTime currentDate = DateTime.now();
+    String formattedDate = "${currentDate.day}/${currentDate.month}/${currentDate.year}";
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -34,15 +38,49 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        // Agrega un fondo de color al cuerpo de la aplicación
-        body: Container(
-          color: Colors.blue, // Cambia el color de fondo aquí
-          child: Center(
-            child: Text(
-              'Muere!',
-              style: TextStyle(fontSize: 24, color: Colors.white),
+        // Utiliza un ListView para colocar el encabezado y la imagen
+        body: ListView(
+          children: [
+            Container(
+              color: Colors.blue, // Cambia el color de fondo aquí
+              child: Center(
+                child: Text(
+                  formattedDate, // Mostrar la fecha actual
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+              ),
             ),
-          ),
+            Center(
+              child: Image.network(
+                'https://images.pexels.com/photos/3585088/pexels-photo-3585088.jpeg',
+                width: MediaQuery.of(context).size.width, // Establece el ancho de la imagen al ancho de la pantalla
+                height: MediaQuery.of(context).size.height, // Establece el alto de la imagen al alto de la pantalla
+                fit: BoxFit.contain, // Ajusta la imagen al tamaño de la pantalla
+              ),
+            ),
+            Center(
+              child: Text(
+                'ESTUDIANTES CANSADOS',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 100), // Ancho máximo del texto
+                child: Text(
+                  'Los estudiantses están agotados debido a la carga de trabajo académico y las responsabilidades extracurriculares. '
+                  'La combinación de exámenes, proyectos, tareas y actividades deportivas ha dejado a los estudiantes con '
+                  'poco tiempo libre y pocas horas de sueño. La falta de descanso y el estrés constante están teniendo un '
+                  'impacto en su bienestar físico y emocional.',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
